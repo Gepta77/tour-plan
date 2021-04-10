af118f328c8369cb19a6379818b0d3de2ed02219
Gepta77 <ogepting@yahoo.de>
2021-04-10 11:25:38 +0200

    create: media rules for hotel section

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const hotelswiper = new Swiper('.hotel-slider', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
+  },
+});
